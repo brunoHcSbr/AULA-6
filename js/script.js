dcad172a303c23f57e67fcf6cc2636d588b737c5
Bruno Henrique <bh089188@gmail.com>
2022-10-18 15:54:25 -0300

    Mudança no Index 3.0

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function exibir(elemento){
+    alert(elemento.innerText);
+}
+const listaTeste = document.querySelectorAll('#lista')
+for (let i = 0; i < lista.length; i++) {
+    list[i].addEventListener('click',function(){exibir(this);});
+    
+}
